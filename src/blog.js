ea7125e87df1d119aa15f184a1b968250b89b6ae
Tomáš Mrázek <tomasmrazek92@gmail.com>
2025-10-09 21:57:27 +0200

    Capped Authors

--- src/blog.js
+++ src/blog.js
@@ -21,6 +21,19 @@ $(document).ready(function () {
           disableOnInteraction: false,
         },
         on: {
+          beforeInit: () => {
+            $(document).ready(function () {
+              $('p[authors]').each(function () {
+                const text = $(this).text().trim();
+                const names = text.split(',').map((name) => name.trim());
+
+                if (names.length > 5) {
+                  const displayNames = names.slice(0, 5).join(', ');
+                  $(this).text(displayNames + ', et\u00A0al.');
+                }
+              });
+            });
+          },
           init: (swiper) => {
             progressLine($('.blog-hero_slider'), swiper);
           },
